4970903136da41a512d31517b98ad051c59b7479
Kavya Raavi <kavyasriraavi@gmail.com>
2025-04-02 22:57:25 +0530

    initial commit

---
+++ swiggy.js
@@ -0,0 +1,6 @@
+// Example: Show an alert on clicking 'Order Now'
+document.querySelectorAll('.btn-primary').forEach(button => {
+    button.addEventListener('click', function() {
+        alert('actually I did connected');
+    });
+});
